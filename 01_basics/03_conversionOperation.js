d84e7d922ce051a9dc65e9c57497a8a62f182465
Harish Malviya <144028633+harishmlv@users.noreply.github.com>
2023-12-01 18:27:21 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Herry"
+
+console.log(typeof score);
+console.log(typeof (score)); 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+True => 1 , False => 0
+"Herry" => NaN
+*/
+
+let isLoggedIn = "herry"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+ 1  => true , 0 => false
+ ""   => false 
+ "Herry" => true
+ */
+
+ let someNumber = 55
+ let stringNumber = String(someNumber) 
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
