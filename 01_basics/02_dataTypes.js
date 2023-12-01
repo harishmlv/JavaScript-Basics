93f9cdef28353e87d76428342b13f9966f3721e7
Harish Malviya <144028633+harishmlv@users.noreply.github.com>
2023-12-01 18:06:53 +0000

    datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3 ) // we are using NodeJS, not browser
+
+console.log("Harish")
+
+let name = "Herry"
+let age = 22
+let isLoggedIn = true
+
+/* Data Types (Premitive)
+ 1. Number => 2 to the power 53
+ 2. bigint => used for big numbers (stock market)
+ 3. String => "Harish"
+ 4. boolean => True/False
+ 5. Null => stand alone value (empty value) example => let age=null
+ 6. undefined => (value not defined yet)
+ 7. symbol => unique (most used in react)
+*/
+/*
+Object
+*/
+console.log(typeof null); // outpull = null // because null is an object
+console.log(typeof undefined); // outpull = undefined 
