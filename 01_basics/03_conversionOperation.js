293351a8440d33f0d2af3836afa98e840efa8ecf
Harish Malviya <144028633+harishmlv@users.noreply.github.com>
2023-12-02 14:47:20 +0000

    conversion operation & comparison done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "Herry"
 
-console.log(typeof score);
-console.log(typeof (score)); 
+//console.log(typeof score);
+//console.log(typeof (score)); 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 /*
 "33" => 33
@@ -16,7 +16,7 @@ True => 1 , False => 0
 
 let isLoggedIn = "herry"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 /*
  1  => true , 0 => false
@@ -26,5 +26,39 @@ console.log(booleanIsLoggedIn);
 
  let someNumber = 55
  let stringNumber = String(someNumber) 
- console.log(stringNumber);
- console.log(typeof stringNumber);
+ //console.log(stringNumber);
+ //console.log(typeof stringNumber);
+
+ // ********** OPERATIONS *********
+
+ let value = 3 
+ let negValue = -value
+//  console.log(negValue);
+
+//  console.log(2+2)
+//  console.log(2-2)
+//  console.log(2*2)
+//  console.log(2**2)
+//  console.log(2/2)
+//  console.log(2%2) 
+ 
+let str1 = "Hello"
+let str2 = " Herry"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log( ( 3 + 2 ) *5 %3 );
+
+// console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num1 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// link to study Prefix and Postfix => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
