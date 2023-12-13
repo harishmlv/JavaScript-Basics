33b0119fc05ab31c3f94cf53445117f8f16b5dfa
Harish Malviya <144028633+harishmlv@users.noreply.github.com>
2023-12-13 19:26:50 +0000

    THEORY OF FUNCTIONS

---
+++ 03_basics/01_functionsTheory.js
@@ -0,0 +1,27 @@
+/**
+ 
+A JavaScript function is a block of code designed to perform a particular task.
+A JavaScript function is executed when "something" invokes it (calls it).
+
+*JavaScript Function Syntax*
+A JavaScript function is defined with the function keyword, followed by a name, followed by parentheses ().
+Function names can contain letters, digits, underscores, and dollar signs (same rules as variables).
+The parentheses may include parameter names separated by commas:
+(parameter1, parameter2, ...)
+The code to be executed, by the function, is placed inside curly brackets: {}
+
+function name(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+
+*FUNCTION RETURN*
+When JavaScript reaches a return statement, the function will stop executing.
+If the function was invoked from a statement, JavaScript will "return" to execute 
+the code after the invoking statement.
+Functions often compute a return value. The return value is "returned" back to the "caller":
+
+*Why Functions?*
+1. With functions you can reuse code
+2. You can write code that can be used many times.
+3. You can use the same code with different arguments, to produce different results.
+ */
