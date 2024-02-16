4c1393da8a42faf6bec1089965f01bb4a4b8f97c
Harish Malviya <144028633+harishmlv@users.noreply.github.com>
2024-02-16 17:55:53 +0000

    class and function

--- 10_classes_and_oops/MyClasses.js
+++ 10_classes_and_oops/MyClasses.js
@@ -18,3 +18,24 @@ class User {
 const chai = new User("chai", "chai@gmail.com", "123")
 console.log(chai.encryptPassword());
 console.log(chai.changeUserName());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
