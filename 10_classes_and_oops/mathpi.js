ab60e486dd9c4a91191c4090b47ec51308b9c488
Harish Malviya <144028633+harishmlv@users.noreply.github.com>
2024-03-04 16:20:50 +0000

    math.pi

--- 10_classes_and_oops/mathpi.js
+++ 10_classes_and_oops/mathpi.js
@@ -20,8 +20,7 @@ console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
 Object.defineProperty(chai, 'name', {
     //writable: false,
-    enumerable: true,
-    
+    enumerable: true,   
 })
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
